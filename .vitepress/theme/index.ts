382c4d4acf697514ffc1f457ef4c5404c3008b28
Jannis Milz <67200746+jannismilz@users.noreply.github.com>
2026-02-04 22:13:45 +0100

    feat: Improve landing page

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,12 +3,14 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+import HomeSections from './HomeSections.vue'
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'home-features-after': () => h(HomeSections)
     })
   },
   enhanceApp({ app, router, siteData }) {
